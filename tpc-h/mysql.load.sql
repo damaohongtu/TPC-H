6bea346cf27f291eb5dc4ce3d8a577641d87db65
tianmao <tianmao1994@yahoo.com>
2018-05-21 13:50:50 +0800

    表间有依赖，调整数据load的顺序

--- tpc-h/mysql.load.sql
+++ tpc-h/mysql.load.sql
@@ -1,17 +1,9 @@
 use tpch;
-
-LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/customer.tbl' INTO TABLE customer FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/orders.tbl'   INTO TABLE orders   FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/lineitem.tbl' INTO TABLE lineitem FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/nation.tbl'   INTO TABLE nation   FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/partsupp.tbl' INTO TABLE partsupp FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
-
-LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/part.tbl'     INTO TABLE part     FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
-
 LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/region.tbl'   INTO TABLE region   FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
-
+LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/nation.tbl'   INTO TABLE nation   FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/part.tbl'     INTO TABLE part     FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
 LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/supplier.tbl' INTO TABLE supplier FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/partsupp.tbl' INTO TABLE partsupp FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/customer.tbl' INTO TABLE customer FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/orders.tbl'   INTO TABLE orders   FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
+LOAD DATA LOCAL INFILE '/home/mao/tpch_workplace/tpc_h_tool/dbgen/lineitem.tbl' INTO TABLE lineitem FIELDS TERMINATED BY '|'  LINES TERMINATED BY '\n';
