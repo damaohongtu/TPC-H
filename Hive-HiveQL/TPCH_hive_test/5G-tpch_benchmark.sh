88e33274fe79384d84153c6f911b986020c95213
SQG <m18813092767@163.com>
2018-06-19 19:31:56 +0800

    Update 5G-tpch_benchmark.sh

--- Hive-HiveQL/TPCH_hive_test/5G-tpch_benchmark.sh
+++ Hive-HiveQL/TPCH_hive_test/5G-tpch_benchmark.sh
@@ -26,24 +26,24 @@ HIVE_TPCH_QUERIES_ALL=( \
 		"tpch-5G/q2_minimum_cost_supplier.hive" \
 		"tpch-5G/q3_shipping_priority.hive" \
 		"tpch-5G/q4_order_priority.hive" \
-        "tpch-5G/q5_local_supplier_volume.hive" \
-        "tpch-5G/q6_forecast_revenue_change.hive" \
-        "tpch-5G/q7_volume_shipping.hive" \
-        "tpch-5G/q8_national_market_share.hive" \
-        "tpch-5G/q9_product_type_profit.hive" \
-        "tpch-5G/q10_returned_item.hive" \
-        "tpch-5G/q11_important_stock.hive" \
-        "tpch-5G/q12_shipping.hive" \
-        "tpch-5G/q13_customer_distribution.hive" \
-        "tpch-5G/q14_promotion_effect.hive" \
-        "tpch-5G/q15_top_supplier.hive" \
-        "tpch-5G/q16_parts_supplier_relationship.hive" \
-        "tpch-5G/q17_small_quantity_order_revenue.hive" \
+                "tpch-5G/q5_local_supplier_volume.hive" \
+        	"tpch-5G/q6_forecast_revenue_change.hive" \
+       		"tpch-5G/q7_volume_shipping.hive" \
+        	"tpch-5G/q8_national_market_share.hive" \
+        	"tpch-5G/q9_product_type_profit.hive" \
+        	"tpch-5G/q10_returned_item.hive" \
+        	"tpch-5G/q11_important_stock.hive" \
+        	"tpch-5G/q12_shipping.hive" \
+        	"tpch-5G/q13_customer_distribution.hive" \
+        	"tpch-5G/q14_promotion_effect.hive" \
+        	"tpch-5G/q15_top_supplier.hive" \
+        	"tpch-5G/q16_parts_supplier_relationship.hive" \
+        	"tpch-5G/q17_small_quantity_order_revenue.hive" \
 		"tpch-5G/q18_large_volume_customer.hive" \
-        "tpch-5G/q19_discounted_revenue.hive" \
-        "tpch-5G/q20_potential_part_promotion.hive" \
-        "tpch-5G/q21_suppliers_who_kept_orders_waiting.hive" \
-        "tpch-5G/q22_global_sales_opportunity.hive" \
+        	"tpch-5G/q19_discounted_revenue.hive" \
+        	"tpch-5G/q20_potential_part_promotion.hive" \
+        	"tpch-5G/q21_suppliers_who_kept_orders_waiting.hive" \
+        	"tpch-5G/q22_global_sales_opportunity.hive" \
 )
 
 
